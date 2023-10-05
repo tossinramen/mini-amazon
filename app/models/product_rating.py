9ef248ee4da75795da187df7e9448b51cdbf47b5
Nick Bayer <ncb39@duke.edu>
2023-10-05 17:37:57 -0400

    Done with Social Guru Milestone 3

--- app/models/product_rating.py
+++ app/models/product_rating.py
@@ -2,20 +2,22 @@ from flask import current_app as app
 
 
 class Product_Rating:  
-    def __init__(self, uid, pid, time_reviewed, description, upvotes, downvotes, stars): 
+    def __init__(self, uid, pid, product_name, description, upvotes, downvotes, stars, time_reviewed,): 
         self.uid = uid
         self.pid = pid
-        self.time_reviewed = time_reviewed
+        self.product_name = product_name
         self.description = description
         self.upvotes = upvotes
         self.downvotes = downvotes
-        self.stars = stars  
+        self.stars = stars
+        self.time_reviewed = time_reviewed  
 
     @staticmethod
     def get_last_5(uid):
         rows = app.db.execute('''
-            SELECT *
-            FROM Product_Rating
+            SELECT pr.uid as uid, pr.pid as pid, p.name as product_name, pr.description, pr.upvotes, pr.downvotes, pr.stars, pr.time_reviewed
+            FROM Product_Rating pr
+            JOIN Products p ON p.id = pr.pid
             WHERE uid = :uid
             ORDER BY time_reviewed DESC
             ''', uid=uid)
@@ -23,3 +25,16 @@ class Product_Rating:
             return None
         else:  
             return [Product_Rating(*row) for row in rows[:5]]
+    
+    @staticmethod
+    def get(id):
+        rows = app.db.execute("""
+            SELECT id, email, firstname, lastname
+            FROM Users
+            WHERE id = :id
+            """, id=id)
+        return User(*(rows[0])) if rows else None
+        
+    
+    
+
