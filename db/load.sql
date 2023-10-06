44e919b32343d7cda77ec84178564dbc5f0b2294
jjl72 <jjl72@duke.edu>
2023-10-05 20:13:35 -0400

    hopefully this works

--- db/load.sql
+++ db/load.sql
@@ -14,8 +14,3 @@ SELECT pg_catalog.setval('public.products_id_seq',
 SELECT pg_catalog.setval('public.purchases_id_seq',
                          (SELECT MAX(id)+1 FROM Purchases),
                          false);
-
-\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.purchases_id_seq',
-                         (SELECT MAX(id)+1 FROM Purchases),
-                         false);
