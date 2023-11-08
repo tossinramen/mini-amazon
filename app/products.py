be396bf9c8155e962b54b77d3d3d22564f305d14
jjl72 <jjl72@duke.edu>
2023-11-08 00:37:38 -0500

    added category filter

--- app/products.py
+++ app/products.py
@@ -9,44 +9,43 @@ from flask import current_app as app
 from flask import Blueprint
 bp = Blueprint('products', __name__)
 
-@bp.route('/get_top_k_products', methods=['POST'])
-def get_top_k_products():
+@bp.route('/get_products', methods=['POST'])
+def get_products():
     k = request.form.get('k')  # Get the value of 'k' from the form
-
 
     # Convert 'k' to an integer
     try:
         k = int(k)
     except ValueError:
         return "Invalid input for 'k'. Please enter a valid number."
-
-    category = 'all'
-    # if request.method == 'POST':
-    #     try:
-    #         category = request.form.get('category')
-    #     except:
-    #         category = all
-    if request.method == 'POST':
-        category = request.form.get('category', 'all')
-
-    if category == 'all':
-        query = '''
-        SELECT id, name, price, description, available, category, image_url
-        FROM products
-        WHERE available = true
-        ORDER BY price DESC
-        LIMIT :limit;
-        '''
-    else:
-        query = '''
-        SELECT id, name, price, description, available, category, image_url
-        FROM products
-        WHERE available = true AND category = :category
-        ORDER BY price DESC
-        LIMIT :limit;
-        '''
-    top_k_products = app.db.execute(query, limit=k)
+    query = '''
+    SELECT id, name, price, description, available, category, image_url
+    FROM products
+    WHERE available = true;
+    '''
+    products = app.db.execute(query)
 
     # Pass the top_k_products data to a new template for displaying the results
-    return render_template('products.html', top_k_products=top_k_products)
+    return render_template('products.html', products=products)
+
+@bp.route('/category_filter', methods=['GET', 'POST'])
+def category_filter():
+    category = request.args.get('category')
+    
+    if category == '--':
+        query = '''
+        SELECT id, name, price, description, available, category, image_url
+        FROM products
+        WHERE available = true;
+        '''
+    else: 
+        query = '''
+        SELECT id, name, price, description, available, category, image_url
+        FROM products
+        WHERE available = true AND category = :category;
+        '''
+
+    category_filter = app.db.execute(query, category=category) 
+    
+    return render_template('products.html', products=category_filter)   
     
