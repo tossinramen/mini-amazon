b541eafc8567134e598adf0189ccb06c39c870b4
vbt3 <vbt3@duke.edu>
2023-12-09 02:53:47 -0500

    completed carts/order pgs w/ comments

--- app/carts.py
+++ app/carts.py
@@ -4,6 +4,7 @@ import datetime
 import csv
 import random
 import hashlib
+from .models.seller_inventory import SellerInventory
 
 from .models.cart import Cart
 from .models.purchase import Purchase
@@ -21,28 +22,31 @@ def cart(uid):
     if count > 0:
         # get current user's cart
         cart_items = Cart.get_items_by_uid(uid)
-    else:
+    else: # user does not exist
         cart_items = None
 
     if cart_items is None:
         cart_items = []  # ensure cart items is always an iterable
 
-    # render cart pg with line items
+    # render cart pg with user cart line items
     return render_template('carts.html', cart_items=cart_items, uid=uid)
 
 @bp.route('/redirect_to_user_cart')
 def redirect_to_user_cart():
+    # get user id from form data and redirect to user cart pg
     user_id = request.form.get('user_id')
     return redirect(url_for('carts.cart', uid=current_user.id))
 
 @bp.route('/update_all_quantities', methods=['GET', 'POST'])
 def update_all_quantities():
-    print(request.form)
+    print(request.form) # debugging
     for key in request.form:
         if key.startswith('quantity_'):
+            # split key to obtain identifiers
             _, id, pid, sid = key.split('_')
-            new_quantity = int(request.form[key])  # Convert to integer
+            new_quantity = int(request.form[key])  # convert qty to integer
 
+            # query to get max avail qty from seller inv
             max_quantity_result = app.db.execute('''
                 SELECT quantity
                 FROM Seller_Inventory
@@ -51,7 +55,8 @@ def update_all_quantities():
                 ''', sid=sid, pid=pid)
 
             if max_quantity_result:
-                max_quantity = max_quantity_result[0][0]  # Extract the integer value
+                max_quantity = max_quantity_result[0][0] # extract integer qty val from res
+                # update cart item qty if within limit
                 if new_quantity <= max_quantity:
                     app.db.execute('''
                         UPDATE CartLineItems
@@ -61,37 +66,46 @@ def update_all_quantities():
                         AND sid = :sid
                         ''', new_quantity=new_quantity, id=id, pid=pid, sid=sid, uid=current_user.id)
                 else:
+                    # error if new qty exceeds avail stock
                     abort(400, "Insufficient inventory for one or more items. Quantities not updated.")
             else:
+                # error if item not in seller inv
                 abort(400, "Item not found in seller inventory.")
-                
+    # back to cart pg after updating qtys
     return redirect(url_for('carts.cart', uid=current_user.id))
+
 # remove an item from cart
 @bp.route('/remove_item/<int:id>/<int:pid>/<int:sid>', methods=['GET', 'POST'])
 def remove_item(id, pid, sid):
+    # delete specified item
     app.db.execute('''
         DELETE FROM CartLineItems
         WHERE id = :id
         AND pid = :pid
         AND sid = :sid
         ''', id=id, pid=pid, sid=sid, uid=current_user.id)
-    flash("Item removed successfully!", 'success')
+    # redirect to cart pg
     return redirect(url_for('carts.cart', uid=current_user.id))
 
 
 
 
-
+# route to view orders for a user
 PER_PAGE = 10
 @bp.route('/orders/<int:uid>')
 def orders(uid):
+    # redirect to login if not authenticated
     if not current_user.is_authenticated or current_user.id != uid:
         return redirect(url_for('users.login'))
     
+    # determine pg number and calc offset for pagination
     page = request.args.get('page', 1, type=int)
     offset = (page - 1) * PER_PAGE
+
+    # retrieve order items
     raw_order_items = get_orders_by_uid(uid, limit=PER_PAGE, offset=offset)
 
+    # processing to generate unique order num
     order_items = []
     used_end_parts = set() 
     for item in raw_order_items:
@@ -119,10 +133,12 @@ def orders(uid):
         item['order_number'] = order_number
         order_items.append(item)
 
+    # calc total num of orders for pagination and render orders pg
     total_orders = get_total_orders_count(uid)
     return render_template('orders.html', order_items=order_items, uid=uid, page=page, per_page=PER_PAGE, total=total_orders)
 
 def get_orders_by_uid(uid, limit, offset):
+    # fetch order details, aggregation
     sql_query = '''
         SELECT pur.id as purchase_id,
                SUM(bli.price * bli.qty) as total_price,
@@ -139,6 +155,7 @@ def get_orders_by_uid(uid, limit, offset):
     result = app.db.execute(sql_query, uid=uid, limit=limit, offset=offset)
     order_items = []
     for row in result:
+        # dict for each order item
         item = {
             'purchase_id': row[0], 
             'total_price': row[1], 
@@ -150,6 +167,7 @@ def get_orders_by_uid(uid, limit, offset):
 
 
 def get_total_orders_count(uid):
+    # counts total num distinct orders for a user
     count_query = '''
         SELECT COUNT(DISTINCT pur.id)
         FROM BoughtLineItems bli
@@ -157,20 +175,24 @@ def get_total_orders_count(uid):
         WHERE pur.uid = :uid
     '''
     total_result = app.db.execute(count_query, uid=uid)
+    # extract count
     total = total_result[0][0] if total_result else 0
     return total
+
 #order details for each order (what purchases are included in the order)
 @bp.route('/order_details/<int:uid>/<int:purchase_id>')
 def order_details(uid, purchase_id):
+    # user auth, match uid
     if not current_user.is_authenticated or current_user.id != uid:
         return redirect(url_for('users.login'))
 
-
+    # retrieve detailed order info and render
     order_details = get_order_details(purchase_id)
-
     return render_template('order_details.html', order_details=order_details, order_number=f"ORDER #{purchase_id}")
-#need fulfillment type IF fulfilled. 
+
+# need fulfillment type IF fulfilled. 
 def get_order_details(purchase_id):
+    # details of each product in a specific purchase
     sql_query = '''
         SELECT pr.name, bli.qty, bli.price, u.firstname, u.lastname, bli.fulfilled, bli.time_fulfilled
         FROM BoughtLineItems bli
@@ -183,6 +205,7 @@ def get_order_details(purchase_id):
     order_details = []
 
     for row in result:
+        # dict for products in order
         detail = {
             'product_name': row[0],
             'quantity': row[1],
@@ -194,36 +217,33 @@ def get_order_details(purchase_id):
         order_details.append(detail)
 
     return order_details
-    return redirect(url_for('carts.cart', uid=current_user.id))
+
 #cart when submitted goes to purchases and orders, needs to be cleared.
 @bp.route('/submit_cart', methods=['POST'])
 def submit_cart():
     user_id = current_user.id
     # fetch cart items
     cart_items = Cart.get_items_by_uid(user_id)
-    '''
-    # check inventory for each item and store updates
-    inventory_updates = []
-
+    # check curr inventory for each product in cart
     for item in cart_items:
-        # fetch current inventory for the item
         inventory_query = 'SELECT quantity FROM Seller_Inventory WHERE pid = :pid AND uid = :uid'
-        current_inventory = app.db.execute(inventory_query, pid=item.pid, uid=item.sid)
-        
-        if current_inventory and current_inventory[0][0] >= item.qty:
-            # prep inventory update if sufficient qty available
-            new_quantity = current_inventory[0][0] - item.qty
-            inventory_updates.append((new_quantity, item.pid, item.sid))
+        curr_inventory = app.db.execute(inventory_query, pid=item.pid, uid=item.sid)
+        new_quantity = curr_inventory[0][0] - item.qty
+
+        # inventory handling
+        if new_quantity == 0:
+            # no more items left, remove from inv and update product availability
+            update_query = ('''DELETE FROM Seller_Inventory WHERE uid = :uid AND pid = :pid''')
+            app.db.execute(update_query, uid = item.sid, pid = item.pid)
+            if SellerInventory.get_by_pid(item.pid) == None:
+                availability_query = ('''UPDATE Products SET available = FALSE WHERE id = :pid''')
+                app.db.execute(availability_query, pid=item.pid)
         else:
-            # inventory insufficient, raise an error, stop the process
-            abort(400, "Insufficient inventory for one or more items. Order not submitted.")
+            # just update inv with new qty
+            update_query = ('''UPDATE Seller_Inventory SET quantity = :new_quantity WHERE uid = :uid AND pid = :pid''')
+            app.db.execute(update_query, uid = item.sid, new_quantity = new_quantity, pid = item.pid)
     
-    # process the order if all items have sufficient inventory
-    for update in inventory_updates:
-        new_quantity, pid, sid = update
-        update_inventory_query = 'UPDATE Seller_Inventory SET quantity = :new_quantity WHERE pid = :pid AND uid = :sid'
-        app.db.execute(update_inventory_query, new_quantity=new_quantity, pid=pid, sid=sid)
-    '''
+    # create purchase record
     id = Cart.get_id_by_uid(user_id)
     Purchase.create(id, user_id)
 
@@ -236,9 +256,8 @@ def submit_cart():
             VALUES(:purchase_id, :sid, :pid, :qty, :price)
         ''', purchase_id=purchase_id, sid=item.sid, pid=item.pid, qty=item.qty, price=item.price)
 
-    # clear cart items
+    # clear cart items after moving to purchase, go to purchases pg
     Cart.clear_cart(user_id)
-
     return redirect(url_for('users.user_purchases', uid=user_id))
 
 @bp.route('/product_details/<int:pid>', methods=['GET', 'POST'])
@@ -251,7 +270,8 @@ def product_details(pid):
     WHERE id = :pid
     '''
     product_result = app.db.execute(product_query, pid=pid)
-
+    
+    # extract and display product details if found
     if product_result:
         name = product_result[0][1]
         price = product_result[0][2]
@@ -272,6 +292,7 @@ def product_details(pid):
 
     seller_info = app.db.execute(seller_query)
 
+    # render detailed product with fetched info
     return render_template('detailed_product.html', name=name,
                            price=price,
                            description=description,
--- app/models/line_item.py
+++ app/models/line_item.py
@@ -1,5 +1,6 @@
 from flask import current_app as app
 
+# LineItem class constructor
 class LineItem:
     def __init__(self, id, sid, pid, qty, price, product_name):
         self.id = id
@@ -9,6 +10,7 @@ class LineItem:
         self.price = price
         self.product_name = product_name
 
+    # get line items by cart id
     @staticmethod
     def get_by_id(id):
         rows = app.db.execute('''
@@ -18,7 +20,9 @@ class LineItem:
             WHERE li.id = :id
             ORDER BY p.name ASC
             ''', id=id)
+        # if no items found
         if not rows:
             return None
-        else:  
+        else:
+            # create LineItem objects for each row, return as list
             return [LineItem(*row) for row in rows]
